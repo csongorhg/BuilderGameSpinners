2c58ad89eb544cd26144a5e13eea1f4d3f01d68f
mordes <csongorhegedus2@indamail.hu>
2017-01-24 14:17:30 +0100

    épületek javítás haladás

--- core/src/com/mygdx/game/PlayingMechanism/Building.java
+++ core/src/com/mygdx/game/PlayingMechanism/Building.java
@@ -7,26 +7,58 @@ package com.mygdx.game.PlayingMechanism;
 public class Building {
 
     //létrehozásnál
-    private int fa = 25;
-    private int ko = 25;
-    private int arany = 25;
-    private int kaja = 25;
+    protected int faLetrehoz = 0;
+    protected int koLetrehoz = 0;
+    protected int aranyLetrehoz = 0;
+    protected int kajaLetrehoz = 0;
+    protected int lakossagLetrehoz = 0;
+
+    //létrehozásnál
+    protected int faLebont = 0;
+    protected int koLebont = 0;
+    protected int aranyLebont = 0;
+    protected int kajaLebont = 0;
+    protected int lakossagLebont = 0;
 
     //naponta
-    private int faNaponta = 0;
-    private int koNaponta = 0;
-    private int aranyNaponta = 0;
-    private int kajaNaponta = 0;
+    protected int faNaponta = 0;
+    protected int koNaponta = 0;
+    protected int aranyNaponta = 0;
+    protected int kajaNaponta = 0;
+    protected int lakossagNaponta = 0;
 
-    public Building(int fa,int ko,int arany,int kaja,int fanaponta,int konaponta,int aranynaponta,int kajanaponta) {
-        this.fa = fa;
-        this.ko = ko;
-        this.arany = arany;
-        this.kaja = kaja;
+    public Building(int faletrehoz,int koletrehoz,int aranyletrehoz,int kajaletrehoz,int lakossagletrehoz,int fanaponta,int konaponta,int aranynaponta,int kajanaponta,int lakossagnaponta,int falebont,int kolebont,int aranylebont,int kajalebont,int lakossaglebont) {
+        faLetrehoz = faletrehoz;
+        koLetrehoz = koletrehoz;
+        aranyLetrehoz = aranyletrehoz;
+        kajaLetrehoz = kajaletrehoz;
+        lakossagLetrehoz = lakossagletrehoz;
 
         faNaponta = fanaponta;
         koNaponta = konaponta;
         aranyNaponta = aranynaponta;
         kajaNaponta = kajanaponta;
+        lakossagNaponta = lakossagnaponta;
+
+        faLebont = falebont;
+        koLebont = kolebont;
+        aranyLebont = aranylebont;
+        kajaLebont = kajalebont;
+        lakossagLebont = lakossaglebont;
+    }
+
+    public int[] getLebont() {
+        int[] t = {faLebont,koLebont,aranyLebont,kajaLebont,lakossagLebont};
+        return t;
+    }
+
+    public int[] getNaponta() {
+        int[] t = {faNaponta,koNaponta,aranyNaponta,kajaNaponta,lakossagNaponta};
+        return t;
+    }
+
+    public int[] getLetrehoz() {
+        int[] t = {faLetrehoz,koLetrehoz,aranyLetrehoz,kajaLetrehoz,lakossagLetrehoz};
+        return t;
     }
 }
