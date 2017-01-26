76a15e45f4412d5b01b91d3515b881ada625d2bf
Kicsi <kissnorbi9935@gmail.com>
2017-01-26 08:36:17 +0100

    épületek

--- core/src/com/mygdx/game/PlayingMechanism/Buildings.java
+++ core/src/com/mygdx/game/PlayingMechanism/Buildings.java
@@ -5,4 +5,21 @@ package com.mygdx.game.PlayingMechanism;
  */
 
 public class Buildings {
+
+    Building haz1 = new Building(-2,-6,-3,0,-5,1,-2,0,0,1,2,0,1,0,0);
+    Building haz2 = new Building(-5,-12,-10,-6,-8,3,-4,0,0,3,5,0,3,1,0);
+    Building haz3 = new Building(-10,-18,-25,-10,-10,5,-8,0,0,5,10,0,10,3,0);
+    Building banya1 = new Building(-2,0,-5,0,-4,0,0,0,4,2,2,0,2,0,0);
+    Building banya2 = new Building(-5,-2,-10,-5,-6,0,-1,0,10,4,5,0,6,2,0);
+    Building banya3 = new Building(-12,-4,-20,-10,-12,0,-2,0,25,6,12,0,10,5,0);
+    Building farm1 = new Building(-1,0,-1,0,-2,0,3,0,0,0,1,0,0,0,0);
+    Building farm2 = new Building(-2,-4,-4,-1,-4,0,8,0,0,0,2,0,2,0,0);
+    Building farm3 = new Building(-3,-8,-8,-3,-6,0,16,0,0,0,3,0,4,1,0);
+    Building faKitermelo1 = new Building(-2,0,0,0,-2,0,0,4,0,0,2,0,0,0,0);
+    Building faKitermelo2 = new Building(-6,-2,-8,-4,-8,0,-1,10,0,-1,6,0,6,1,0);
+    Building faKitermelo3 = new Building(-12,-4,-16,-8,-10,0,-2,25,0,-2,12,0,8,4,0);
+    Building kut = new Building(0,0,-5,-5,0,0,0,0,0,0,0,0,2,2,0);
+    Building kikepzo = new Building(-10,-15,-15,-15,-15,0,0,0,0,0,10,0,5,5,0);
+
+
 }
