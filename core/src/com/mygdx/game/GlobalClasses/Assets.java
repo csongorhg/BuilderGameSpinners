2963b1d01f26b5fbd90fba52b137e736b7b817d7
Vince <egyed.vince1998@gmail.com>
2017-02-19 17:01:53 +0100

    hamu-k helyrerakása a mappaszerkezetben assetsben való loadolásuk assets kicsit átrendezve(esztétika érdekében)

--- core/src/com/mygdx/game/GlobalClasses/Assets.java
+++ core/src/com/mygdx/game/GlobalClasses/Assets.java
@@ -132,16 +132,13 @@ public class Assets {
 
 
 	//ATLAS
-	public static final AssetDescriptor<TextureAtlas> EXPLOSION_TEXTUREATLAS
-			= new AssetDescriptor<TextureAtlas>("Explosion/explosion.atlas", TextureAtlas.class);
 	public static final AssetDescriptor<TextureAtlas> FIRE_TEXTUREATLAS
 			= new AssetDescriptor<TextureAtlas>("Tiles/tuz.atlas", TextureAtlas.class);
 
 
 
 	//TEXTURE
-	public static final AssetDescriptor<Texture> TEST_TEXTURE
-			= new AssetDescriptor<Texture>("TestImg/ratyitutu.png", Texture.class);
+	//menu
 	public static final AssetDescriptor<Texture> SOUND
 			= new AssetDescriptor<Texture>("Menu/sound.png", Texture.class);
 	public static final AssetDescriptor<Texture> NOSOUND
@@ -152,6 +149,9 @@ public class Assets {
 			= new AssetDescriptor<Texture>("Menu/wall.png", Texture.class);
 	public static final AssetDescriptor<Texture> GRID
 			= new AssetDescriptor<Texture>("Menu/racs.png", Texture.class);
+
+
+	//tiles
 	public static final AssetDescriptor<Texture> FOG
 			= new AssetDescriptor<Texture>("Tiles/kod.png", Texture.class);
 	public static final AssetDescriptor<Texture> GRASS_BLOCK
@@ -166,33 +166,22 @@ public class Assets {
 			= new AssetDescriptor<Texture>("Tiles/ko_1.png", Texture.class);
 	public static final AssetDescriptor<Texture> STONE2_BLOCK
 			= new AssetDescriptor<Texture>("Tiles/ko_2.png", Texture.class);
-
-	public static final AssetDescriptor<Texture> HOUSE
-			= new AssetDescriptor<Texture>("Buildings/haz.png", Texture.class);
-	public static final AssetDescriptor<Texture> STONE_WORKER
-			= new AssetDescriptor<Texture>("Buildings/kofejto.png", Texture.class);
-	public static final AssetDescriptor<Texture> WOOD_WORKER
-			= new AssetDescriptor<Texture>("Buildings/favago.png", Texture.class);
-	public static final AssetDescriptor<Texture> CITY_HALL
-			= new AssetDescriptor<Texture>("Buildings/var.png", Texture.class);
-	public static final AssetDescriptor<Texture> MILL
-			= new AssetDescriptor<Texture>("Buildings/malom.png", Texture.class);
-	public static final AssetDescriptor<Texture> MILLMEZO
-			= new AssetDescriptor<Texture>("Buildings/mezo.png", Texture.class);
-	public static final AssetDescriptor<Texture> BRIDGE
-			= new AssetDescriptor<Texture>("Buildings/hid.png", Texture.class);
-	public static final AssetDescriptor<Texture> FISHDOCK
-			= new AssetDescriptor<Texture>("Buildings/halasz.png", Texture.class);
-	public static final AssetDescriptor<Texture> BARRACK
-			= new AssetDescriptor<Texture>("Buildings/kikepzo.png", Texture.class);
-	public static final AssetDescriptor<Texture> WATER_WELL
-			= new AssetDescriptor<Texture>("Buildings/kut.png", Texture.class);
-	public static final AssetDescriptor<Texture> HAMU
-			= new AssetDescriptor<Texture>("Buildings/hamu.png", Texture.class);
-	public static final AssetDescriptor<Texture> HAMUSNOW
-			= new AssetDescriptor<Texture>("Buildings/hamusnow.png", Texture.class);
+	public static final AssetDescriptor<Texture> KIJELOLES
+			= new AssetDescriptor<Texture>("Tiles/kijeloles.png", Texture.class);
+	//havas tiles
+	public static final AssetDescriptor<Texture> FUSNOW_BLOCK
+			= new AssetDescriptor<Texture>("Tiles/havas/fuSnow.png", Texture.class);
+	public static final AssetDescriptor<Texture> FASNOW_BLOCK
+			= new AssetDescriptor<Texture>("Tiles/fa_2.png", Texture.class);
+	public static final AssetDescriptor<Texture> VIZSNOW_BLOCK
+			= new AssetDescriptor<Texture>("Tiles/havas/vizSnow.png", Texture.class);
+	public static final AssetDescriptor<Texture> KO1_SNOW
+			= new AssetDescriptor<Texture>("Tiles/havas/ko_1_Snow.png", Texture.class);
+	public static final AssetDescriptor<Texture> KO2_SNOW
+			= new AssetDescriptor<Texture>("Tiles/havas/ko_2_Snow.png", Texture.class);
 
 
+	//interface, interface ikonok
 	public static final AssetDescriptor<Texture> BANYA
 			= new AssetDescriptor<Texture>("Interface/banya.png", Texture.class);
 	public static final AssetDescriptor<Texture> BARAKK
@@ -223,10 +212,6 @@ public class Assets {
 			= new AssetDescriptor<Texture>("Interface/hid.png", Texture.class);
 	public static final AssetDescriptor<Texture> REDX
 			= new AssetDescriptor<Texture>("Interface/redX.png", Texture.class);
-	public static final AssetDescriptor<Texture> DG
-			= new AssetDescriptor<Texture>("Interface/downgrade.png", Texture.class);
-	public static final AssetDescriptor<Texture> UG
-			= new AssetDescriptor<Texture>("Interface/upgrade.png", Texture.class);
 	public static final AssetDescriptor<Texture> FIGHT
 			= new AssetDescriptor<Texture>("Interface/fight.png", Texture.class);
 	public static final AssetDescriptor<Texture> FIGHTBLUE
@@ -239,7 +224,7 @@ public class Assets {
 			= new AssetDescriptor<Texture>("Interface/sminusz.png", Texture.class);
 	public static final AssetDescriptor<Texture> PLUSZ
 			= new AssetDescriptor<Texture>("Interface/splusz.png", Texture.class);
-
+	//barrackban képezhető egységek
 	public static final AssetDescriptor<Texture> CANNON_MAN
 			= new AssetDescriptor<Texture>("Interface/cannon.png", Texture.class);
 	public static final AssetDescriptor<Texture> BOW_MAN
@@ -250,19 +235,32 @@ public class Assets {
 			= new AssetDescriptor<Texture>("Interface/horse.png", Texture.class);
 
 
-	public static final AssetDescriptor<Texture> KIJELOLES
-			= new AssetDescriptor<Texture>("Tiles/kijeloles.png", Texture.class);
-
-	public static final AssetDescriptor<Texture> FUSNOW_BLOCK
-			= new AssetDescriptor<Texture>("Tiles/havas/fuSnow.png", Texture.class);
-	public static final AssetDescriptor<Texture> FASNOW_BLOCK
-			= new AssetDescriptor<Texture>("Tiles/fa_2.png", Texture.class);
-	public static final AssetDescriptor<Texture> VIZSNOW_BLOCK
-			= new AssetDescriptor<Texture>("Tiles/havas/vizSnow.png", Texture.class);
-	public static final AssetDescriptor<Texture> KO1_SNOW
-			= new AssetDescriptor<Texture>("Tiles/havas/ko_1_Snow.png", Texture.class);
-	public static final AssetDescriptor<Texture> KO2_SNOW
-			= new AssetDescriptor<Texture>("Tiles/havas/ko_2_Snow.png", Texture.class);
+	//építmények
+	public static final AssetDescriptor<Texture> HOUSE
+			= new AssetDescriptor<Texture>("Buildings/haz.png", Texture.class);
+	public static final AssetDescriptor<Texture> STONE_WORKER
+			= new AssetDescriptor<Texture>("Buildings/kofejto.png", Texture.class);
+	public static final AssetDescriptor<Texture> WOOD_WORKER
+			= new AssetDescriptor<Texture>("Buildings/favago.png", Texture.class);
+	public static final AssetDescriptor<Texture> CITY_HALL
+			= new AssetDescriptor<Texture>("Buildings/var.png", Texture.class);
+	public static final AssetDescriptor<Texture> MILL
+			= new AssetDescriptor<Texture>("Buildings/malom.png", Texture.class);
+	public static final AssetDescriptor<Texture> MILLMEZO
+			= new AssetDescriptor<Texture>("Buildings/mezo.png", Texture.class);
+	public static final AssetDescriptor<Texture> BRIDGE
+			= new AssetDescriptor<Texture>("Buildings/hid.png", Texture.class);
+	public static final AssetDescriptor<Texture> FISHDOCK
+			= new AssetDescriptor<Texture>("Buildings/halasz.png", Texture.class);
+	public static final AssetDescriptor<Texture> BARRACK
+			= new AssetDescriptor<Texture>("Buildings/kikepzo.png", Texture.class);
+	public static final AssetDescriptor<Texture> WATER_WELL
+			= new AssetDescriptor<Texture>("Buildings/kut.png", Texture.class);
+	public static final AssetDescriptor<Texture> HAMU
+			= new AssetDescriptor<Texture>("Buildings/hamu.png", Texture.class);
+	public static final AssetDescriptor<Texture> VIZ_HAMU
+			= new AssetDescriptor<Texture>("Buildings/vizhamu.png", Texture.class);
+	//havas építmények
 	public static final AssetDescriptor<Texture> FAVAGOSNOW
 			= new AssetDescriptor<Texture>("Buildings/havas/favagoSnow.png", Texture.class);
 	public static final AssetDescriptor<Texture> HALASZSNOW
@@ -283,7 +281,13 @@ public class Assets {
 			= new AssetDescriptor<Texture>("Buildings/havas/mezoSnow.png", Texture.class);
 	public static final AssetDescriptor<Texture> VARSNOW
 			= new AssetDescriptor<Texture>("Buildings/havas/varSnow.png", Texture.class);
+	public static final AssetDescriptor<Texture> HAMUSNOW
+			= new AssetDescriptor<Texture>("Buildings/havas/hamusnow.png", Texture.class);
+	public static final AssetDescriptor<Texture> VIZ_HAMUSNOW
+			= new AssetDescriptor<Texture>("Buildings/havas/vizSnowhamu.png", Texture.class);
 
+
+	//button
 	public static final AssetDescriptor<Texture> TEXTBUTTONUP
 			= new AssetDescriptor<Texture>("Buttons/textbuttonup.png", Texture.class);
 	public static final AssetDescriptor<Texture> TEXTBUTTONDOWN
@@ -309,11 +313,12 @@ public class Assets {
 
 
 		//TEXTURA
-		manager.load(TEST_TEXTURE);
 		manager.load(SOUND);
 		manager.load(NOSOUND);
 		manager.load(GATEWALL);
 		manager.load(GRID);
+		manager.load(WALL);
+
 		manager.load(FOG);
 		manager.load(GRASS_BLOCK);
 		manager.load(WATER_BLOCK);
@@ -321,25 +326,34 @@ public class Assets {
 		manager.load(TREE3_BLOCK);
 		manager.load(STONE_BLOCK);
 		manager.load(STONE2_BLOCK);
+		manager.load(FUSNOW_BLOCK);
+		manager.load(VIZSNOW_BLOCK);
+		manager.load(FASNOW_BLOCK);
+		manager.load(KO1_SNOW);
+		manager.load(KO2_SNOW);
+
 		manager.load(HOUSE);
 		manager.load(STONE_WORKER);
 		manager.load(WOOD_WORKER);
 		manager.load(CITY_HALL);
 		manager.load(BANYA);
 		manager.load(BARAKK);
-		manager.load(ARANY);
 		manager.load(FAVAGO);
 		manager.load(HALASZ);
-		manager.load(HAZ);
 		manager.load(KUT);
+
+		manager.load(ARANY);
+		manager.load(HAZ);
 		manager.load(MEAT);
 		manager.load(MEZO);
 		manager.load(PEOPLE);
 		manager.load(STONE);
 		manager.load(WOOD);
+
 		manager.load(FAHATTER);
-		manager.load(WALL);
 		manager.load(KIJELOLES);
+		manager.load(REDX);
+
 		manager.load(MILL);
 		manager.load(MILLMEZO);
 		manager.load(BRIDGE);
@@ -347,25 +361,20 @@ public class Assets {
 		manager.load(FISHDOCK);
 		manager.load(WATER_WELL);
 		manager.load(HID);
-		manager.load(REDX);
-		manager.load(DG);
-		manager.load(UG);
+
 		manager.load(CANNON_MAN);
 		manager.load(HORSE_MAN);
 		manager.load(SWORD_MAN);
 		manager.load(BOW_MAN);
+
 		manager.load(FIGHT);
 		manager.load(FIGHTBLUE);
 		manager.load(FIGHTGREEN);
 		manager.load(FIGHTRED);
+
 		manager.load(MINUSZ);
 		manager.load(PLUSZ);
 
-		manager.load(FUSNOW_BLOCK);
-		manager.load(VIZSNOW_BLOCK);
-		manager.load(FASNOW_BLOCK);
-		manager.load(KO1_SNOW);
-		manager.load(KO2_SNOW);
 		manager.load(FAVAGOSNOW);
 		manager.load(HALASZSNOW);
 		manager.load(HIDSNOW);
@@ -378,6 +387,8 @@ public class Assets {
 		manager.load(HAZSNOW);
 		manager.load(HAMU);
 		manager.load(HAMUSNOW);
+		manager.load(VIZ_HAMU);
+		manager.load(VIZ_HAMUSNOW);
 
 		//BUTTONS
 		manager.load(TEXTBUTTONDOWN);
@@ -395,12 +406,13 @@ public class Assets {
 
 		//BETÜ
 		manager.load(ACMEREGULAR_FONT50);
-		manager.load(ACMEREGULAR_FONT100);
+
 		manager.load(ACMEREGULAR_FONT10);
 		manager.load(ACMEREGULAR_FONT25);
-
 		manager.load(ACMEREGULAR_FONT75);
 		manager.load(ACMEREGULAR_FONT80);
+		manager.load(ACMEREGULAR_FONT100);
+		manager.load(ACMEREGULAR_FONT140);
 	}
 
     public static void afterLoaded() {
