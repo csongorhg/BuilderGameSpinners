51d001515239d71bce0855a76b99c3f30056f13a
mordes <csongorhegedus2@indamail.hu>
2017-02-25 20:34:45 +0100

    mentés

--- core/src/com/mygdx/game/Play/PlayStage.java
+++ core/src/com/mygdx/game/Play/PlayStage.java
@@ -814,6 +814,7 @@ abstract public class PlayStage extends MyStage implements GestureDetector.Gestu
     @Override
     public void dispose() {
         super.dispose();
+        statisticSave();
         barbarianAttackStage.dispose();
         ingameMenu.dispose();
         tipStage.dispose();
